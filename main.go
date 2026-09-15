52cf7d02cf995710074c94443efa6b9269e504dc
Vladimir <fazberfredi635@gmail.com>
2026-09-16 02:14:07 +0300

    Commit 3

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import ()
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"time"
+)
+
+func menu(){
+	fmt.Println("Выберите действие")
+	fmt.Println("1 - пинг сайтов")
+	fmt.Println("2 - показать отчет")
+	fmt.Println("0 - выход")
+}
 
 func main(){
-	
+	fmt.Println("Приветсвую в NetPulseCore!")
+	Scanner := bufio.NewScanner(os.Stdin)
+	for{
+		menu()
+		Scanner.Scan()
+		InputUser := Scanner.Text()
+		switch InputUser{
+		case "1":
+			//Метод для пинга
+			time.Sleep(1 * time.Second)
+		case "2":
+			//Отчет
+			time.Sleep(1 * time.Second)
+		case "0":
+			fmt.Println("Спасибо за использование моей программы")
+			return
+		default:
+			fmt.Println("Команда не распознана")
+			time.Sleep(1 * time.Second)
+		}
+	}	
 }
